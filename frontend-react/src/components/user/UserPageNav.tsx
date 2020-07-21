e5763bc6d2c231c059d491b108fd447990503bb4
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-07-21 17:17:12 +0300

    user page nav skeleton

---
+++ frontend-react/src/components/user/UserPageNav.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Box } from "@chakra-ui/core"
+
+interface UserPageNavProps {}
+
+const UserPageNav: React.FC<UserPageNavProps> = ({}) => {
+  return <Box>asd</Box>
+}
+
+export default UserPageNav
